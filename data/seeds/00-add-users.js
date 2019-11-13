b499eb5aad7dafed338763b2cfccb359b3d80799
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-13 15:32:46 +0100

    seed file created, db populated

---
+++ data/seeds/00-add-users.js
@@ -0,0 +1,16 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        { username: 'user1', password: '1234', department: 'finance'},
+        { username: 'user2', password: '1234', department: 'finance'},
+        { username: 'user3', password: '1234', department: 'finance'},
+        { username: 'user4', password: '1234', department: 'finance'},
+        { username: 'user5', password: '1234', department: 'finance'},
+        { username: 'paco', password: '1234', department: 'accounts'},
+      ]);
+    });
+};
